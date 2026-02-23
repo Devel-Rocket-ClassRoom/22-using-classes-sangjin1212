d8530c2a1a0580b1fe6a38f784c2ef3b191b2e78
sangjin1212 <sangjin411@gmail.com>
2026-02-23 19:58:45 +0900

    윤상진

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -6,9 +6,17 @@ BankAccount bankAccount = new BankAccount
 };
 
 Console.WriteLine($"[계좌 정보]({bankAccount.AccountNumber}) ({bankAccount.OwnerName} - 잔액 {bankAccount.Balance})");
+
 class BankAccount
 {
+    int sum = 0;
+
     public string AccountNumber { get; set; }
     public string OwnerName { get; set; }
     public int Balance { get; set; }
+
+    public static int Deposit()
+    { 
+        
+    }
 }
